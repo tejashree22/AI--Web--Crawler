7cdd72d222769a7cfad5a6c81eb7c796b327e0e4
tejashree22 <65409857+tejashree22@users.noreply.github.com>
2024-11-15 18:02:41 -0500

    Update hgyjg.py

--- hgyjg.py
+++ hgyjg.py
@@ -1,7 +1,4 @@
-from transformers import pipeline
+#from transformers import pipeline
 
 # Download the model and cache it locally
-pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", framework="pt")
-
-https://www.amazon.com/Best-Sellers/zgbs
-Enter the prompt (e.g., 'extract industry reports'): "Extract top 10 products in Electronics"
+#pipeline("summarization", model="sshleifer/distilbart-cnn-12-6", framework="pt")
